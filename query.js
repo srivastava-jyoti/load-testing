8a84915b0dffd6725331f99ec0f2ba0b0ea177e2
srivastava-jyoti <jyoti.srivastava@coloredcow.in>
2024-07-16 16:24:05 +0530

    added multiple queries

---
+++ query.js
@@ -0,0 +1,303 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '1m', target: 50 }, // Ramp up to 10 users over 1 minute
+    { duration: '3m', target: 30 }, // Stay at 10 users for 3 minutes
+    { duration: '1m', target: 0 },  // Ramp down to 0 users over 1 minute
+  ],
+};
+
+const query = `
+  query GetStateData($selectedState: String!) {
+    statedata: states(pagination: { page: 1, pageSize: 1000 }) {
+      data {
+        id
+        attributes {
+          name
+        }
+      }
+    }
+    states: states(filters: { name: { eq: $selectedState } }, pagination: { page: 1, pageSize: 1000 }) {
+      data {
+        id
+        attributes {
+          name
+          rti_forms {
+            data {
+              id
+              attributes {
+                RTI_Name
+                RTI_memo_number
+                scanned_file_location
+                Scanned_files {
+                  __typename
+                  data {
+                    id
+                    attributes {
+                      previewUrl
+                      url
+                    }
+                  }
+                }
+                District_Name
+                district {
+                  data {
+                    id
+                    attributes {
+                      name
+                    }
+                  }
+                }
+                blur_area_height
+                margin_top
+                Start_Date
+                End_Date
+                prison {
+                  data {
+                    id
+                    attributes {
+                      name
+                    }
+                  }
+                }
+                covid_19_vaccination_rti_prisoner {
+                  data {
+                    id
+                    attributes {
+                      Male1stDose_19_to_45
+                      Female1stDose_19_to_45
+                      Male1stDose_45_to_60
+                      Female1stDose_45_to_60
+                      Male1stDose_Age60_and_above
+                      Female1stDose_Age60_and_above
+                      MaleBothDoses_19_to_45
+                      FemaleBothDoses_19_to_45
+                      MaleBothDoses_45_to_60
+                      FemaleBothDoses_45_to_60
+                      MaleBothDoses_Age60_and_above
+                      FemaleBothDoses_Age60_and_above
+                      Male1stDose_19_to_45_Dropdown
+                      Female1stDose_19_to_45_Dropdown
+                      Male1stDose_45_to_60_Dropdown
+                      Female1stDose_45_to_60_Dropdown
+                      Male1stDose_Age60_and_above_dropdown
+                      Female1stDose_Age60_and_above_dropdown
+                      MaleBothDoses_19_to_45_Dropdown
+                      FemaleBothDoses_19_to_45_Dropdown
+                      MaleBothDoses_45_to_60_Dropdown
+                      FemaleBothDoses_45_to_60_Dropdown
+                      MaleBothDoses_Age60_and_above_Dropdown
+                      FemaleBothDoses_Age60_and_above_Dropdown
+                      total_prisoner_1stDose
+                      total_prisoner_bothDose
+                      total_prisoner_1stDose_Dropdown
+                      total_prisoner_bothDose_Dropdown
+                      vaccination_documents
+                    }
+                  }
+                }
+                covid_19_vaccination_rti_prison_staffs {
+                  data {
+                    id
+                    attributes {
+                      Male1stDose_19_to_45
+                      Female1stDose_19_to_45
+                      Male1stDose_45_to_60
+                      Female1stDose_45_to_60
+                      Male1stDose_Age60_and_above
+                      Female1stDose_Age60_and_above
+                      MaleBothDoses_19_to_45
+                      FemaleBothDoses_19_to_45
+                      MaleBothDoses_45_to_60
+                      FemaleBothDoses_45_to_60
+                      MaleBothDoses_Age60_and_above
+                      FemaleBothDoses_Age60_and_above
+                      Male1stDose_19_to_45_Dropdown
+                      Female1stDose_19_to_45_Dropdown
+                      Male1stDose_45_to_60_Dropdown
+                      Female1stDose_45_to_60_Dropdown
+                      Male1stDose_Age60_and_above_dropdown
+                      Female1stDose_Age60_and_above_dropdown
+                      MaleBothDoses_19_to_45_Dropdown
+                      FemaleBothDoses_19_to_45_Dropdown
+                      MaleBothDoses_45_to_60_Dropdown
+                      FemaleBothDoses_45_to_60_Dropdown
+                      MaleBothDoses_Age60_and_above_Dropdown
+                      FemaleBothDoses_Age60_and_above_Dropdown
+                      total_staff_1stDose
+                      total_staff_bothDose
+                      vaccination_documents
+                      total_staff_1stDose_Dropdown
+                      total_staff_1stDose_Dropdown
+                      FemaleBothDoses_Age60_and_above_Dropdown
+                    }
+                  }
+                }
+                covid_19_hpc_rtis {
+                  data {
+                    id
+                    attributes {
+                      Regular_Parole
+                      Emergency_Parole
+                      Special_Parole
+                      Permanent_Parole
+                      Premature_Release
+                      Bail
+                      Transfer_to_Open_prison
+                      Temporary_prison
+                      Regular_Parole_Dropdown
+                      Emergency_Parole_Dropdown
+                      Special_Parole_Dropdown
+                      Permanent_Parole_dropdown
+                      Premature_Release_dropdown
+                      Bail_Dropdown
+                      Transfer_to_Open_prison_Dropdown
+                      Temporary_prison_Dropdown
+                      released_on_hpc_recommendation
+                      minutes_provided
+                      minutes
+                      mom_information {
+                        minutes_of_meetings
+                        Date
+                      }
+                    }
+                  }
+                }
+                covid_19_testing_rtis {
+                  data {
+                    id
+                    attributes {
+                      total_prisoner1
+                      total_prisoner2
+                      total_prisoner3
+                      total_prisoner4
+                      date1
+                      date2
+                      date3
+                      date4
+                      prison_capacity
+                      Number_of_Medical_Staff
+                      Full_time_doctors_male
+                      Full_time_doctors_Female
+                      visiting_doctors_male
+                      visiting_doctors_Female
+                      specialist_male
+                      specialist_female
+                      weekly_visits_by_medicalStaff
+                      total_prisoner1_Dropdown
+                      total_prisoner2_Dropdown
+                      total_prisoner3_Dropdown
+                      total_prisoner4_Dropdown
+                      prison_capacity_Dropdown
+                      Number_of_Medical_Staff_Dropdown
+                      Full_time_doctors_male_Dropdown
+                      Full_time_doctors_Female_dropdown
+                      visiting_doctors_male_dropdown
+                      visiting_doctors_Female_Dropdown
+                      specialist_male_Dropdown
+                      specialist_female_Dropdown
+                      weekly_visits_by_medicalStaff_Dropdown
+                      prison_capacity
+                      prison_capacity_Dropdown
+                      weekly_visits_by_medicalStaff
+                      weekly_visits_by_medicalStaff_Dropdown
+                      nearest_medical_center
+                      emergency_nearest_medical_center
+                      other_medical_staff
+                    }
+                  }
+                }
+                covid_19_testing_rti_prisoners {
+                  data {
+                    id
+                    attributes {
+                      Testing_RAT
+                      Testing_RT_PCR
+                      prisoners_tested
+                      prisoners_Infected
+                      Treated
+                      Shifted
+                      Recovered
+                      Succumbed
+                      tested_multiple_times
+                      Testing_RAT_Dropdown
+                      Testing_RT_PCR_Dropdown
+                      prisoners_tested_Dropdown
+                      prisoners_Infected_dropdown
+                      Treated_dropdown
+                      Shifted_Dropdown
+                      Recovered_Dropdown
+                      Succumbed_Dropdown
+                      tested_multiple_times_Dropdown
+                    }
+                  }
+                }
+                covid_19_testing_rti_prison_staffs {
+                  data {
+                    id
+                    attributes {
+                      Testing_RAT
+                      Testing_RT_PCR
+                      prisonStaff_tested
+                      Infected
+                      Treated
+                      Shifted
+                      Recovered
+                      Succumbed
+                      total_prison_staff
+                      Testing_RAT_Dropdown
+                      Testing_RT_PCR_Dropdown
+                      prisonStaff_tested_Dropdown
+                      Infected_dropdown
+                      Treated_dropdown
+                      Shifted_Dropdown
+                      Recovered_Dropdown
+                      Succumbed_Dropdown
+                      total_prison_staff_Dropdown
+                      people_tested_outside
+                      People_Tested_Outside_Dropdown
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    meta {
+      pagination {
+        page
+        pageSize
+      }
+    }
+  }
+}
+`;
+
+const states = ["West Bengal", "Maharashtra", "Karnataka", "Bihar", "Uttar Pradesh", "Uttrakhand"];
+
+export default function () {
+  const url = 'https://uat-cms.paar.org.in/graphql';
+
+  for (let state of states) {
+    let variables = {
+      selectedState: state,
+    };
+
+    let res = http.post(url, JSON.stringify({ query, variables }), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    check(res, {
+      'status is 200': (r) => r.status === 200,
+      'response time is < 500ms': (r) => r.timings.duration < 500,
+    });
+
+    sleep(1); // Adjust sleep time based on your testing needs
+  }
+}
--- script.js
+++ script.js
@@ -1,59 +1,27 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
+import { Counter } from 'k6/metrics';
 
-export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 10,
-  // A string specifying the total duration of the test run.
-  duration: '30s',
+export let errorRate = new Counter('errors');
 
-  // The following section contains configuration options for execution of this
-  // test script in Grafana Cloud.
-  //
-  // See https://grafana.com/docs/grafana-cloud/k6/get-started/run-cloud-tests-from-the-cli/
-  // to learn about authoring and running k6 test scripts in Grafana k6 Cloud.
-  //
-  // cloud: {
-  //   // The ID of the project to which the test is assigned in the k6 Cloud UI.
-  //   // By default tests are executed in default project.
-  //   projectID: "",
-  //   // The name of the test in the k6 Cloud UI.
-  //   // Test runs with the same name will be grouped.
-  //   name: "script.js"
-  // },
-
-  // Uncomment this section to enable the use of Browser API in your tests.
-  //
-  // See https://grafana.com/docs/k6/latest/using-k6-browser/running-browser-tests/ to learn more
-  // about using Browser API in your test scripts.
-  //
-  // scenarios: {
-  //   // The scenario name appears in the result summary, tags, and so on.
-  //   // You can give the scenario any name, as long as each name in the script is unique.
-  //   ui: {
-  //     // Executor is a mandatory parameter for browser-based tests.
-  //     // Shared iterations in this case tells k6 to reuse VUs to execute iterations.
-  //     //
-  //     // See https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ for other executor types.
-  //     executor: 'shared-iterations',
-  //     options: {
-  //       browser: {
-  //         // This is a mandatory parameter that instructs k6 to launch and
-  //         // connect to a chromium-based browser, and use it to run UI-based
-  //         // tests.
-  //         type: 'chromium',
-  //       },
-  //     },
-  //   },
-  // }
+export let options = {
+    stages: [
+        { duration: '30s', target: 50 }, // Ramp-up to 10 users over 30 seconds
+        { duration: '1m', target: 50 },  // Stay at 10 users for 1 minute
+        { duration: '10s', target: 0 },  // Ramp-down to 0 users over 10 seconds
+    ],
 };
 
-// The function that defines VU logic.
-//
-// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
-// about authoring k6 scripts.
-//
-export default function() {
-  http.get('http://uat.paar.org.in/map');
-  sleep(1);
+export default function () {
+    let res = http.get('http://uat.paar.org.in/map');
+    let success = check(res, {
+        'status is 200': (r) => r.status === 200,
+        'response time is < 500ms': (r) => r.timings.duration < 500,
+    });
+
+    if (!success) {
+        errorRate.add(1);
+    }
+
+    sleep(1);
 }
